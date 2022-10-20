d4f6f8ec1f709a071b61ef971acd1fc31e228f64
Daniel235-web <samueledaniel235@gmail.com>
2022-10-20 05:35:42 +0000

    Daniel

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -5,14 +5,16 @@
  * @n: the number
  * Return: value of the last digit
  */
-int print_last_digit(int n)
+int print_last_digit(int nld)
 {
-	int ld = n % 10;
+	int pld;
 
-	if (ld < 0)
-		ld *= -1;
+	pld = (nld % 10);
 
-	_putchar(ld + '0');
-
-	return (0);
+	if (pld < 0)
+	{
+		pld = (-1 * pld);
+	}
+	_putchar(pld + '0');
+	return (pld);
 }
