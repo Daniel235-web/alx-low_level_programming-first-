4f0519ae12bde5910d48709874c8965473a38f9a
Daniel235-web <samueledaniel235@gmail.com>
2022-10-20 07:05:08 +0000

    Daniel

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,24 +5,27 @@
  */
 void times_table(void)
 {
-	int num, mult, prod;
+	int c, d, e;
 
-	for (num = 0; num <= 9; num++)
+	for (c = 0; c < 10; c++)
 	{
-		_putchar(0);
-
-		for (mult = 1; mult <= 9; mult++)
+		for (d = 0; d < 10; d++)
 		{
-			_putchar(',');
-			_putchar(' ');
+			e = d * c;
 
-			prod = num * mult;
-			if (prod <= 9)
-				_putchar(' ');
-			else
-				_putchar((prod / 10) + '0');
-			_putchar((prod % 10) + '0');
+			if (e / 10 != 0)
+			{
+				_putchar(e / 10 + '0');
+			}
+			_putchar(e % 10 + '0');
+
+			if (d != 9)
+			{
+				_putchar(',');
+				_putchar('\t');
+			}
 		}
 		_putchar('\n');
 	}
 }
+
