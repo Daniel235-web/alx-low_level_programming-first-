da8ced9eede281a81b557d29cd0ba86f42183167
Daniel235-web <samueledaniel235@gmail.com>
2022-10-24 21:32:10 +0000

    Daniel

--- 0x05-pointers_arrays_strings/_putchar.c
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -1,11 +1,11 @@
 #include <unistd.h>
 
 /**
- * _putchar - writes the charcter c to stdout
+ * _putchar - writes the character c to stdout
  * @c: The character to print
  *
- * Return: on sucess 1.
- * On error, -1 is returned, ans errno is set appropriately
+ * Return: On success 1.
+ * On error, -1 is returned, and erno is set appropriately
  */
 int _putchar(char c)
 {
