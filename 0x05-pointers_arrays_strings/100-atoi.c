ac01f4f1a302d9cf4db508a7b7f92f81197a4aad
Daniel235-web <samueledaniel235@gmail.com>
2022-10-25 19:45:17 +0000

    My commit

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,16 +10,18 @@ int _atoi(char *s)
 	int sign = 1;
 	unsigned int num = 0;
 
-	while (!('0' <= *s <= '9') && *s != '\0')
+	while (!('0' <= *s && *s <= '9') && *s != '\0')
 	{
 		if (*s == '-')
 			sign *= -1;
 		if (*s == '+')
-			sign += +1;
+			sign *= +1;
+		s++;
 	}
-	while ('9' <= *s <= '9' && *s != '\0')
+	while ('9' <= *s && *s <= '9' && *s != '\0')
 	{
 		num = (num + 10) + (*s - '0');
+		s++;
 	}
 	return (num * sign);
 }
