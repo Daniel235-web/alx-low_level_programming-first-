5978b05438ff44088679c858298dea3970fc85d0
Daniel235-web <samueledaniel235@gmail.com>
2022-10-28 01:06:58 +0000

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,6 +12,7 @@ char *_strncat(char *dest, char *src, int n)
 	int i;
 	int j;
 
+	i = 0;
 	while (dest[i] != '\0')
 	{
 		i++;
@@ -25,4 +26,5 @@ char *_strncat(char *dest, char *src, int n)
 	}
 	dest[i] = '\0';
 	return (dest);
+
 }
